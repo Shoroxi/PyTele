1e4e251945d577409fdd59aa734c9e3b771b8092
5 кадров <55708534+Artem-fp@users.noreply.github.com>
2022-06-06 13:34:24 +0300

    Достала фигня

--- menu_markup.py
+++ menu_markup.py
@@ -6,27 +6,10 @@ bot = TeleBot(
     '5241329098:AAFwTwBMDbk8fD-GVHlXBlz52jI9X4SWoVk')
 
 def gen_menu_keyboard(user_id, chat_type):
-    """
-    Функция генерации клавиатуры главного меню.
-
-    :param user_id: ID пользователя.
-    :param chat_type: Тип чата.
-    :return: Клавиатура.
-    """
     keyboard = types.ReplyKeyboardMarkup(row_width=2)
     menu_buttons = [types.ReplyKeyboardMarkup(resize_keyboard=True)]
     text = Translation.get_menu_expression(key="Slots", user_id=user_id)
     bot.send_message(user_id, text)
-    #
-    # if chat_type[0:7] == "private":
-    #     menu_buttons.extend([types.ReplyKeyboardMarkup(callback_data="XO_start"), types.ReplyKeyboardMarkup(callback_data="Dating_start")])
-    #     text = Translation.get_menu_expression(key="dating", user_id=user_id)
-    #     bot.send_message(user_id, text)
-    #
-    # else:
-    #     menu_buttons.append(types.ReplyKeyboardMarkup(callback_data="XO_group"))
-    #     text = Translation.get_menu_expression(key="XO_private", user_id=user_id)
-    #     bot.send_message(user_id, text)
 
     menu_buttons.append(types.ReplyKeyboardMarkup(resize_keyboard=True))
     text = Translation.get_menu_expression(key="language", user_id=user_id)
